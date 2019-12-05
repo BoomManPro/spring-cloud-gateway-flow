e179a6cf3eeec39e46a64767bdaa542bf673a6d3
wangqimeng <wangqimeng@mininglamp.com>
2019-12-05 14:27:26 +0800

    add module spring-cloud-gateway-flow-common

---
+++ spring-cloud-gateway-flow-common/src/main/java/cn/boommanpro/common/ResultVo.java
@@ -0,0 +1,39 @@
+package cn.boommanpro.common;
+
+import lombok.Data;
+
+/**
+ * @author wangqimeng
+ * @date 2019/11/7 14:40
+ */
+@Data
+public class ResultVo<T> {
+
+    private String code;
+
+    private String message;
+
+    private T data;
+
+    private static final String SUCCESS = "SUCCESS";
+
+    private static final String ERROR = "ERROR";
+
+    private ResultVo(String code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ResultVo<T> ofSuccess(T data) {
+        return new ResultVo<>(SUCCESS, null, data);
+    }
+
+    public static <T> ResultVo<T> ofSuccess() {
+        return new ResultVo<>(SUCCESS, null, null);
+    }
+
+    public static <T> ResultVo<T> ofError(String message) {
+        return new ResultVo<>(ERROR, message, null);
+    }
+}
